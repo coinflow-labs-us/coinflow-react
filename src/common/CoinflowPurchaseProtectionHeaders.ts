b76c992b3da0be9f01f8403eb7510f18c02f6de4
coinflow-labs-us <ben@coinflowlabs.app>
2026-04-29 13:13:31 +0000

    Update from https://github.com/coinflow-labs-us/coinflow/commit/bcd9fd6fc50ad0c1d483d1724afb8bddce4a97af

--- src/common/CoinflowPurchaseProtectionHeaders.ts
+++ src/common/CoinflowPurchaseProtectionHeaders.ts
@@ -3,6 +3,7 @@ import {getProtectionSessionId} from './CoinflowProtectionSession';
 
 export const DEVICE_ID_HEADER = 'x-device-id';
 export const SESSION_ID_HEADER = 'x-session-id';
+export const REFERRER_HEADER = 'x-coinflow-referrer';
 
 export type CoinflowProtectionHeadersType = {
   [DEVICE_ID_HEADER]: string | null;
@@ -13,5 +14,24 @@ export function getCoinflowProtectionHeaders(): CoinflowProtectionHeadersType {
   return {
     [DEVICE_ID_HEADER]: getNSureDeviceId(),
     [SESSION_ID_HEADER]: getProtectionSessionId(),
+  };
+}
+
+function getReferrer(): string | null {
+  try {
+    return document.referrer || null;
+  } catch {
+    return null;
+  }
+}
+
+export type CoinflowPurchaseHeadersType = CoinflowProtectionHeadersType & {
+  [REFERRER_HEADER]: string | null;
+};
+
+export function getCoinflowPurchaseHeaders(): CoinflowPurchaseHeadersType {
+  return {
+    ...getCoinflowProtectionHeaders(),
+    [REFERRER_HEADER]: getReferrer(),
   };
 }
